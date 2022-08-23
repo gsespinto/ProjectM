f2d6b374b9d8cfea72d28800c48db1058a64f131
Gonçalo Pinto <69385490+gsespinto@users.noreply.github.com>
2022-08-23 11:39:08 +0100

    Giant minion vomit;

---
+++ Source/ProjectM/GiantEnemy.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GiantEnemy.h"
+#include "Enemy.h"
+#include "Components/SceneComponent.h"
+
+AGiantEnemy::AGiantEnemy()
+{
+	SpawnPoint = CreateDefaultSubobject<USceneComponent>("Spawn Point");
+	SpawnPoint->SetupAttachment(GetMesh(), TEXT("head"));
+}
+
+void AGiantEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AGiantEnemy::SpawnAction()
+{
+	if (SpawnMontage == nullptr)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Missing spawn montage on Giant Enemy!"));
+		return;
+	}
+
+	GetMesh()->GetAnimInstance()->Montage_Play(SpawnMontage);
+}
+
+void AGiantEnemy::SpawnMinions()
+{
+	if (MinionClass == nullptr)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Missing minion class on Giant Enemy!"));
+		return;
+	}
+
+	GetWorld()->SpawnActor<ACharacter>(MinionClass, SpawnPoint->GetComponentLocation(), GetActorRotation(), FActorSpawnParameters());
+}
---
+++ Source/ProjectM/GiantEnemy.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Enemy.h"
+#include "GiantEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTM_API AGiantEnemy : public AEnemy
+{
+	GENERATED_BODY()
+
+public:
+	AGiantEnemy();
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+
+private:
+	UFUNCTION(BlueprintCallable)
+		void SpawnAction();
+
+ 	UFUNCTION(BlueprintCallable)
+		void SpawnMinions();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Minions", meta = (AllowPrivateAccess = "true"))
+		class USceneComponent* SpawnPoint;
+
+	UPROPERTY(EditAnywhere, Category="Minions")
+		UAnimMontage* SpawnMontage;
+
+	UPROPERTY(EditAnywhere, Category = "Minions")
+		TSubclassOf<class AEnemy> MinionClass;
+};
